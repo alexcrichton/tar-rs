40efd16fdc779e10b52f3757a8559ab5e4dc8ac7
Paul Colomiets <paul@colomiets.name>
2016-05-05 00:13:37 +0300

    Fix `entry.header().path()` to `entry.path()` in examples

--- examples/list.rs
+++ examples/list.rs
@@ -12,6 +12,6 @@ fn main() {
     let mut arch = Archive::new(stdin());
     for file in arch.entries().unwrap() {
         let f= file.unwrap();
-        println!("{}", f.header().path().unwrap().display());
+        println!("{}", f.path().unwrap().display());
     }
 }
