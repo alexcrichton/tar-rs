b9cffc8735c9617eb69bf942c748ace0091ded35
liushuyu <liushuyu011@gmail.com>
2021-07-22 08:32:21 -0600

    builder: add support for archiving special files on Unix (#246)
    * builder: add support for archiving special files on Unix
    
    Added support for archiving character, block and fifo files on Unix,
    this is useful when you want to take a whole system backup or
    re-archiving an extracted system image
    
    * tests: display system error for the special files test
    
    * builder: address comments ...
    
    * attempt to reduce `stat` calls
    
    * builder: merge `append_fs_special` into `append_special`

--- tests/all.rs
+++ tests/all.rs
@@ -1212,3 +1212,36 @@ fn read_only_directory_containing_files() {
     let mut ar = Archive::new(&contents[..]);
     assert!(ar.unpack(td.path()).is_ok());
 }
+
+// This test was marked linux only due to macOS CI can't handle `set_current_dir` correctly
+#[test]
+#[cfg(target_os = "linux")]
+fn tar_directory_containing_special_files() {
+    use std::env;
+    use std::ffi::CString;
+
+    let td = t!(TempBuilder::new().prefix("tar-rs").tempdir());
+    let fifo = td.path().join("fifo");
+
+    unsafe {
+        let fifo_path = t!(CString::new(fifo.to_str().unwrap()));
+        let ret = libc::mknod(fifo_path.as_ptr(), libc::S_IFIFO | 0o644, 0);
+        if ret != 0 {
+            libc::perror(fifo_path.as_ptr());
+            panic!("Failed to create a FIFO file");
+        }
+    }
+
+    t!(env::set_current_dir(td.path()));
+    let mut ar = Builder::new(Vec::new());
+    // append_path has a different logic for processing files, so we need to test it as well
+    t!(ar.append_path("fifo"));
+    t!(ar.append_dir_all("special", td.path()));
+    // unfortunately, block device file cannot be created by non-root users
+    // as a substitute, just test the file that exists on most Unix systems
+    t!(env::set_current_dir("/dev/"));
+    t!(ar.append_path("loop0"));
+    // CI systems seem to have issues with creating a chr device
+    t!(ar.append_path("null"));
+    t!(ar.finish());
+}
